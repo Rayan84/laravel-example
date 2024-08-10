37894a2615e8ac9b052e0d1b015567d481788d57
Rayan Rassam <rayan_84@live.com>
2024-08-10 13:29:13 +0200

    Add first and last name to user migration

--- database/migrations/2024_08_10_112233_add_first_name_to_users_table.php
+++ database/migrations/2024_08_10_112233_add_first_name_to_users_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->text('body');
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('first_name');
         });
     }
 
@@ -24,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('first_name');
+        });
     }
 };
