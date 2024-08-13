29dcf1de4e6c17b1e23204225a383bbbfb78e38c
Rayan Rassam <rayan_84@live.com>
2024-08-13 10:12:15 +0200

    Create Employer model and migration

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
+Use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model {
+    use HasFactory;
     protected $table = 'jobs_listings';
-
     protected $fillable = ['title', 'salary'];
 }
 
