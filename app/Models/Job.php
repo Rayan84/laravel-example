299532dfec511d8f74d67760ce0d3ea251e8f9af
Rayan Rassam <rayan_84@live.com>
2024-08-13 14:24:31 +0200

    Edit EmployerFactory

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,16 +1,17 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
 Use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model {
     use HasFactory;
+
     protected $table = 'jobs_listings';
+
     protected $fillable = ['title', 'salary'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
-
-        
-
-
-?>
--- database/factories/EmployerFactory.php
+++ database/factories/EmployerFactory.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace Database\Factories;
-
+use App\Models\Employer;
 use Illuminate\Database\Eloquent\Factories\Factory;
-
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employer>
  */
-class JobFactory extends Factory
+//Extends: Factory<Employer>
+class EmployerFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -17,9 +17,7 @@ class JobFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => fake()->jobTitle(),
-            'employer_id' => Employer::factory(),
-            'salary' => '50,5000 USD'
+            'name' => fake()->company()
         ];
     }
 }
