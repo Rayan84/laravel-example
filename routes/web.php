7e5b4890e1fd9c1d22bd092c3f3b0b8afb22f1dd
Rayan Rassam <rayan_84@live.com>
2024-08-17 10:38:48 +0200

    Add paginate() to limit queries

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::all();
+    $jobs = Job::with('employer')->paginate(3);
 
     return view('jobs', [
         'jobs' => $jobs
