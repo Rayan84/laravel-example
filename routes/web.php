3fd016a9fbc0f65edf487c0b3c9f6a3e3bc12156
Rayan Rassam <rayan_84@live.com>
2024-08-24 08:50:52 +0200

    Add delete form

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,36 @@ Route::post('/jobs', function () {
     return view('jobs.edit', ['job' => $job]);
  });
 
+ Route::patch('/jobs/{id}', function ($id) {
+    //validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    // update
+    $job = Job::findOrFail($id);
+    // $job->salary = request('salary');
+    // $job->title = request('title');
+    // $job->save();
+    $job->update([
+        'title'=> request('title'),
+        'salary' => request('salary')
+    ]);
+    //authorize
+    //redirect
+    return redirect('/jobs/' .$job->id);
+ });
+
+ Route::delete('jobs/{id}', function ($id) {
+    //authorize
+    //delete
+    $job = Job::findOrFail($id);
+    $job->delete();
+    //redirect
+    return redirect('/jobs');
+ });
+
 Route::get('/contact', function () {
     return view('contact');
 });
