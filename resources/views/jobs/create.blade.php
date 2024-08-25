34377bb3154acf7f4609e4387185f7ea884e0717
Rayan Rassam <rayan_84@live.com>
2024-08-25 17:21:16 +0200

    Create form-error, form-input and form-label blades

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -9,9 +9,10 @@
       <div class="border-b border-gray-900/10 pb-12">
         <h2 class="text-base font-semibold leading-7 text-gray-900">Create a New Job</h2>
         <p class="mt-1 text-sm leading-6 text-gray-600">We just need a handful of details from you</p>
+
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div class="sm:col-span-4">
-            <label for="title" class="block text-sm font-medium leading-6 text-gray-900" required>Title</label>
+            <x-form-label for="title">Title</x-form-label>
             <div class="mt-2">
               <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                 <input type="text" name="title" id="title" class="block pl-4 flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Shift Supervisor" required>
--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 
 Route::view('/', 'home');
+Route::resource('jobs', JobController::class);
 
-
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-Route::post('/jobs', [JobController::class, 'store']);
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-Route::delete('jobs/{job}', [JobController::class, 'destroy']);
-
-
-Route::view('/contact', 'contact');
-
+Route::view('/contact', 'contact'); 
